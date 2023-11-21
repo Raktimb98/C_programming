c3972a4906a1c1eae8f9f1d6f46edb7b8724f4f6
Raktimb98 <raktimb181@gmail.com>
2023-11-21 19:30:02 +0530

    Add function

--- Table.c
+++ Table.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
+void printTable(int n);
 int main(void){
     int n;
     printf("Enter a number:");
     scanf("%d",&n);
-    for (int i = 10; i >=1; i--)
+    printTable(n);
+    return 0;
+}
+void printTable(int n){
+
+    for (int i = 1; i <=10; i++)
     {
         printf("%d\n",n*i);
     }
-    return 0;
 }
