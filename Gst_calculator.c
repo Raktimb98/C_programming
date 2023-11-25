1075473241833052a12cf6a304fd14a691eba355
Raktimb98 <raktimb181@gmail.com>
2023-11-25 08:12:06 +0530

    Add GST calculator

---
+++ Gst_calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void calculatePrice(float value);
+
+int main(void) {
+    float value,gst;
+
+    printf("Enter a value: ");
+    scanf("%f", &value);
+    printf ("Enter gst value");
+    scanf("%d",&gst);
+
+    calculatePrice(value);
+
+    return 0;
+}
+
+void calculatePrice(float value) {
+    float finalPrice;
+    finalPrice = value + (0.18 * value);
+    printf("Your final price is: %f\n", finalPrice);
+}
+//04.29.17 video timeline
