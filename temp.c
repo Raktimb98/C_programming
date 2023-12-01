575f9f2e04106fc8af360ccc0a8c50e7de35d0e4
Raktimb98 <raktimb181@gmail.com>
2023-12-01 12:56:28 +0530

    Add new fetures

--- temp.c
+++ temp.c
@@ -1,14 +1,74 @@
 #include <stdio.h>
-float temp(float c);
+
+void fahrenheit();
+void centigrade();
+void kelvin();
+
 int main()
 {
-    float a;
-    printf("Enter a num:");
-    scanf("%f", &a);
-    printf("Temprature is %f\n", temp(a));
+    char dialog;
+    printf("Enter f for Fahrenheit or c for Centigrade and k for kelvin:");
+    scanf(" %c", &dialog);
+    if (dialog == 'f')
+    {
+        fahrenheit();
+    }
+    else if (dialog == 'c')
+    {
+        centigrade();
+    }
+    else if (dialog == 'k')
+    {
+        kelvin();
+    }
+    else
+    {
+        printf("Wrong type of value!");
+    }
+
+    return 0;
 }
-float temp(float c)
+
+void fahrenheit()
 {
-    int fer = (c * 9.0 / 5.0) + 32;
-    return fer;
+    float temp1, value;
+    printf("Enter the temperature in Fahrenheit:");
+    scanf("%f", &temp1);
+    value = (temp1 - 32) * 5 / 9;
+    printf("The current temperature is %f Celsius\n", value);
+}
+
+void centigrade()
+{
+    float temp2, value2;
+    printf("Enter the temprature in centigrade:");
+    scanf("%f", &temp2);
+    value2 = (temp2 * 9 / 5) + 32;
+    printf("The current temprature is %f centigrade\n", value2);
+}
+void kelvin()
+{
+    char dialog;
+    printf("From which scale you want to convert c for centigrade or f for Fahrenheit:");
+    scanf(" %c", &dialog);
+    if (dialog == 'c')
+    {
+        float temp3, value3;
+        printf("Enter the temperature in kelvin:");
+        scanf("%f", &temp3);
+        value3 = temp3 + 273.15;
+        printf("The current temperature is %f Kelvin\n", value3);
+    }
+    else if (dialog == 'f')
+    {
+        float temp4, value4;
+        printf("Enter the temperature in kelvin:");
+        scanf("%f", &temp4);
+        value4 = (temp4 - 32) * 5 / 9 + 273.15;
+        printf("The current temperature is %f Kelvin\n", value4);
+    }
+    else
+    {
+        printf("Something went wrong!");
+    }
 }
