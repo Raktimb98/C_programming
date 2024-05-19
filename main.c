a3950ecf4b817d09be6620d520705fc24d474550
Raktim Biswas <raktimb181@gmail.com>
2024-05-19 10:41:34 +0530

    add new features

--- main.c
+++ main.c
@@ -25,13 +25,25 @@ int main(void)
     //     printf("%d\t",i);
     //     i++;
     // } while (i<=5);
-// int num;
-// printf("Enter a number:-");
-// scanf("%d",&num);
-// int sum=0;
-// for(int i=0;i<=num;i++){
-//     sum+=i;
-// }
-// printf("Sum of first %d natural numbers is %d",num,sum);
+    // int num;
+    // printf("Enter a number:-");
+    // scanf("%d",&num);
+    // int sum=0;
+    // for(int i=0;i<=num;i++){
+    //     sum+=i;
+    // }
+    // printf("Sum of first %d natural numbers is %d",num,sum);
+    int n;
+    do
+    {
+        printf("Enter a number:- ");
+        scanf("%d", &n);
+        if (n % 2 != 0)
+
+        {
+            break;
+        }
+
+    } while (1);
     return 0;
 }
