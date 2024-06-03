67eaa75e123a94fbc3cfe4bbc40ceb3efb90b6eb
Raktim Biswas <raktimb181@gmail.com>
2024-06-03 14:01:25 +0530

    percentage calculator TM 500

---
+++ main.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+// int calculate(int a, int b, int c, int d, int e);
+// int main(void)
+// {
+//     int a, b, c, d, e;
+//     printf("Enter three numbers: ");
+//     scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+//     printf("The result is: %d\n", calculate(a, b, c, d, e));
+//     return 0;
+// }
+// int calculate(int a, int b, int c, int d, int e)
+// {
+//     // int percentage = ((a+b+c)/3);
+//     //(300*100/500)
+//     int percentage = ((a + b + c + d + e) * 100) / (500);
+//     return percentage;
+// }
