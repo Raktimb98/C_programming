3a5935651ccda6a80bde42f874a5805b2eee8f44
Raktim Biswas <raktimb181@gmail.com>
2024-06-11 15:39:17 +0530

    array

--- main.c
+++ main.c
@@ -1,29 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-// Function prototype should match the definition
-void array(int a[], int n);
-
-int main(void){
-    printf("Enter the number of elements: ");
+int main(void) {
     int n;
+    printf("Enter a number: ");
     scanf("%d", &n);
 
-    int a[n];
-    printf("Enter the elements: ");
+    int a[n];  // Declare the array after n has been initialized
+
+    printf("Enter %d numbers:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-    // Call the array function with the array and its size
-    array(a, n);
+    printf("Your values are: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
 
     return 0;
-}
-
-// Function definition
-void array(int a[], int n) {
-    for (int i = 0; i < n; i++) {
-        // Printing the value of each element in the array
-        printf("index: %d, value: %d\n", i, a[i]);
-    }
 }
