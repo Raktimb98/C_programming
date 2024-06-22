a48e58f29ffd5e2c23938051edf5b93bd1122d46
Raktim Biswas <raktimb181@gmail.com>
2024-06-22 09:46:48 +0530

    create a file

--- main.c
+++ main.c
@@ -1,16 +1,6 @@
 #include<stdio.h>
-void printString(char arr[]);
 int main(void){
-printf("Enter a name:- ");
-char ch[100];
-scanf("%s",ch);
-printString(ch);
+FILE *fptr;
+fptr = fopen("README.md","r");
 return 0;
-}
-void printString(char arr[]){
-    for (int i = 0; arr[i] !='\0'; i++)
-    {
-        printf("%c",arr[i]);
-    }
-    
 }
